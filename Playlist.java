d1aa069a4e37c676e7e1ce6124e727d8d305fc37
Lucia Colwell <lcolwell23@students.polytechnic.org>
2023-01-11 14:58:59 -0800

    Added toString methods to Java object

---
+++ Playlist.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Playlist {
+    private ArrayList playlist;
+    public Playlist(){
+        playlist = new ArrayList<Song>();
+    }
+
+    public void addSong
+    
+}
