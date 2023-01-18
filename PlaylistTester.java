f5edd6582d95c1651227646633c95e7dc2214d56
Sofia Segal <ssegal23@students.polytechnic.org>
2023-01-17 17:43:28 -0800

    added gaga

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -5,6 +5,14 @@ public class PlaylistTester {
         Song friday = new Song("Friday I'm in Love", "The Cure", true, 3, 35);
         Playlist set = new Playlist();
         set.addSong(friday);
+
+        System.out.println("Adding \"Bad Romance\" by LADY GAGA...");
+        Song gaga = new Song("Bad Romance", "LADY GAGA", true, 4, 53);
+        set.addSong(gaga);
+        
+        
+
+
         
 
     }
